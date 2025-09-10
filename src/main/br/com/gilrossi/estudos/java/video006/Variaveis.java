4ca7eb02c57d4c36e9ca3e817ed99c22a7d6da8e
Gil Rossi <gil.rossi.aguiar@hotmail.com>
2025-09-10 11:54:13 -0300

    Variaveis

---
+++ src/main/br/com/gilrossi/estudos/java/video006/Variaveis.java
@@ -0,0 +1,25 @@
+package video006;
+
+/*
+ * Números inteiros
+ * byte 8bits
+ * short 16 bits
+ * int 32 bits
+ * long 64 bits
+ * 
+ * Caractere ASCII
+ * char 16 bits
+ * 
+ * Números  decimais (sempre utilizar o f quando for usar o float
+ * senão o compilador considera como double)
+ * float 32 bits (usar o f para relacionar ao float no valor da variavel)
+ * double 64 bits
+ * 
+ * Booleano
+ * boolean 1 bit (true or false)
+ * 
+ */
+
+public class Variaveis {
+
+}
