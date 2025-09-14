4a756c0cc3ed42a796e3f2f1ecb90fa30546f012
Gil Rossi <gil.rossi.aguiar@hotmail.com>
2025-09-14 11:23:17 -0300

    Diferenças entre == e equals

--- src/main/br/com/gilrossi/estudos/java/video007/TiposReferencia.java
+++ src/main/br/com/gilrossi/estudos/java/video007/TiposReferencia.java
@@ -3,14 +3,22 @@ package video007;
 public class TiposReferencia {
 	public static void main(String[] args) {
 		
-		var conta = new Soma(2.2, 3.3);		
-		var conta2 = new Soma(2.5, 3.3);
-		var conta3 = new Soma(2.2, 3.3);
+		var conta = new Soma(2.5, 7.5);		
+		var conta2 = new Soma(2.5, 2.5);
+		var conta3 = new Soma(2.5, 3.5);
 		
 		System.out.println(conta == conta3);
 		System.out.println(conta.contaConjunta == conta3.contaConjunta);
 		System.out.println(conta.contaConjunta.equals(conta3.contaConjunta));
 		System.out.println(conta.equals(conta3));
+		System.out.println(conta.somando());
+		System.out.println(conta2.somando());
+		System.out.println(conta3.somando());
+		
+		int a = 1;
+		int b = 1;
+		
+		System.out.println(a == b);
 		
 	}
 
